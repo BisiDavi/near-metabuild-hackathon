6ade93e6903d6e18456ad67d1c6e0e2b0e689a86
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 03:52:15 +0100

    hero updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,11 @@ export default function Hero() {
         <div className="container mx-auto flex flex-col py-8 text-center text-white">
           <h1 className="text-3xl">{heroContent.home.main} </h1>
           <h6 className="text-lg">{heroContent.home.info}</h6>
-          
+          <button className="0 relative my-4 mx-auto flex w-auto rounded-md bg-white px-4 py-1.5 font-bold text-blue-500 hover:bg-gray-200">
+            Get Started
+            <span className="absolute -right-1.5 -top-1 inline-flex h-3  w-3 animate-ping rounded-full bg-red-500"></span>
+            <span className="absolute -right-1.5 -top-1 inline-flex h-3 w-3 rounded-full bg-red-500"></span>
+          </button>
         </div>
       </div>
       <HeroWave />
