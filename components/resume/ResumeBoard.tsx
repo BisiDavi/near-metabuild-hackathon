afef3e158ba5b23a74e71363a2117511bd831d7b
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 15:11:39 +0100

    resume board & provider added

---
+++ components/resume/ResumeBoard.tsx
@@ -0,0 +1,9 @@
+import ResumeBoardProvider from '@/providers/ResumeBoardProvider';
+
+export default function ResumeBoard() {
+  return (
+    <ResumeBoardProvider>
+      <section>ResumeBoard</section>
+    </ResumeBoardProvider>
+  );
+}
