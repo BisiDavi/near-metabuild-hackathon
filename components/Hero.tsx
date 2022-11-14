bcd4c76176cf7848f9619edac7685747ada16399
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 05:22:06 +0100

    nav updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import HeroWave from '@/components/HeroWave';
 import HeroCards from '@/components/HeroCards';
 import heroContent from '@/json/hero.json';
@@ -9,11 +11,13 @@ export default function Hero() {
         <div className="container mx-auto flex flex-col py-10 text-center text-white">
           <h1 className="text-3xl">{heroContent.home.main} </h1>
           <h6 className="text-lg">{heroContent.home.info}</h6>
-          <button className="0 relative my-8 mx-auto flex w-auto rounded-md bg-white px-4 py-1.5 font-bold text-blue-500 hover:bg-gray-200">
-            Get Started
-            <span className="absolute -right-1.5 -top-1 inline-flex h-3  w-3 animate-ping rounded-full bg-red-500"></span>
-            <span className="absolute -right-1.5 -top-1 inline-flex h-3 w-3 rounded-full bg-red-500"></span>
-          </button>
+          <Link href="/cv/build">
+            <button className="0 relative my-8 mx-auto flex w-auto rounded-md bg-white px-4 py-1.5 font-bold text-blue-500 hover:bg-gray-200">
+              Get Started
+              <span className="absolute -right-1.5 -top-1 inline-flex h-3  w-3 animate-ping rounded-full bg-red-500"></span>
+              <span className="absolute -right-1.5 -top-1 inline-flex h-3 w-3 rounded-full bg-red-500"></span>
+            </button>
+          </Link>
         </div>
       </div>
       <HeroWave />
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,14 +8,12 @@ import navcontent from '@/json/nav.json';
 export default function Nav() {
   const pathname = usePathname();
 
-  console.log('pathname', pathname);
-
   return (
     <nav className="flex w-3/4 justify-end">
       <ul className="list-style-none flex items-center space-x-4">
         {navcontent.map((item) => {
           const activeLink = pathname?.includes(item.link)
-            ? 'text-gray-400 border-b font-bold'
+            ? 'text-white-400 border-b font-bold'
             : 'hover:border-b-2 hover:border-gray-400 hover:text-red-500 hover:ease-in';
           return (
             <li key={item.title} className={`${activeLink} px-2 duration-300 `}>
