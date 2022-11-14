d7b3b0112cbff1f2beb92f7b636163753a05d7cf
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 05:25:31 +0100

    layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import '@/styles/globals.css';
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import '@/styles/globals.css';
-
 export default function Page() {
   return <div>page</div>;
 }
