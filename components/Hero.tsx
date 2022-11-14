8d32b714bb08d0afcfbb1a2c244336824d8b94e3
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 03:40:05 +0100

    nav updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,16 @@
 import HeroWave from '@/components/HeroWave';
+import heroContent from '@/json/hero.json';
 
 export default function Hero() {
   return (
     <section>
+      <div className="content bg-skyblue h-52">
+        <div className="container mx-auto flex flex-col py-8 text-center text-white">
+          <h1 className="text-3xl">{heroContent.home.main} </h1>
+          <h6 className="text-lg">{heroContent.home.info}</h6>
+          
+        </div>
+      </div>
       <HeroWave />
     </section>
   );
