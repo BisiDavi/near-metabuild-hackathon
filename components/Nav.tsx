6d09304be7f102c311781bf3d5b20898b4c53f33
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 04:35:39 +0100

    jobs page added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,20 +1,18 @@
 import Link from 'next/link';
 
 import navcontent from '@/json/nav.json';
-import toSlug from '@/lib/toSlug';
 
 export default function Nav() {
   return (
     <nav className="flex w-3/4 justify-end">
       <ul className="list-style-none flex items-center space-x-4">
         {navcontent.map((item) => {
-          const linkSlug = toSlug(item);
           return (
             <li
-              key={item}
+              key={item.title}
               className="px-2 duration-300 hover:border-b-2 hover:border-gray-400 hover:text-red-500 hover:ease-in"
             >
-              <Link href={linkSlug}>{item}</Link>
+              <Link href={item.link}>{item.title}</Link>
             </li>
           );
         })}
