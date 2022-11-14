6b2882d103c07478ca1b465f87baef3d59182036
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 02:54:57 +0100

    toSlug added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import navcontent from '@/json/nav.json';
 
 export default function Nav() {
@@ -5,8 +7,11 @@ export default function Nav() {
     <nav className="flex w-3/4 justify-end">
       <ul className="list-style-none flex items-center space-x-4">
         {navcontent.map((item) => (
-          <li key={item} className="hover:text-red-500">
-            {item}
+          <li
+            key={item}
+            className="px-2 duration-300 hover:border-b-2 hover:border-gray-400 hover:text-red-500 hover:ease-in"
+          >
+            <Link href={item}>{item}</Link>
           </li>
         ))}
       </ul>
