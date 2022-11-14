74c251ad724e11d387269a4e94ee780aa9ecd0e4
BisiDavi <oludavidconnect@gmail.com>
2022-11-14 04:53:31 +0100

    herocards updated

--- components/HeroCards.tsx
+++ components/HeroCards.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 import heroContent from '@/json/hero.json';
 
 export default function HeroCards() {
+  const pathname = usePathname();
+  const cardArray = heroContent.cards.filter((card) =>
+    pathname?.includes(card.icon),
+  );
+  const cards = cardArray.length === 0 ? heroContent.cards : cardArray;
   return (
     <ul className="mx-auto -mt-80 flex w-1/2 list-none flex-col">
-      {heroContent.cards.map((card) => (
+      {cards.map((card) => (
         <li
           key={card.icon}
           className="cards my-2.5 rounded-lg  border bg-white py-4 px-6 shadow-lg"
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Logo() {
   return (
-    <div className="flex items-center border-2 border-white px-4 py-1 text-white hover:border-black hover:bg-black hover:text-white">
+    <Link
+      href="/"
+      className="flex items-center border-2 border-white px-4 py-1 text-white hover:border-black hover:bg-black hover:text-white"
+    >
       CV-NEAR
-    </div>
+    </Link>
   );
 }
