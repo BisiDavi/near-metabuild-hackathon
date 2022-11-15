6c78af74395deae5fd5b45918be583496b8fa47a
BisiDavi <oludavidconnect@gmail.com>
2022-11-15 10:34:27 +0100

    separator element updated

--- components/resume/DragWrapper.tsx
+++ components/resume/DragWrapper.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
-
-export default function DragWrapper() {
-  return (
-    <div>DragWrapper</div>
-  )
+import { memo, PropsWithChildren } from 'react';
+import { useDrag } from 'react-dnd';
+interface Props {
+  id: string;
+  type: string;
+  position: {
+    left: number;
+    top: number;
+  };
+  style: {
+    type: 'dotted' | 'bold';
+    color: string;
+  };
 }
+
+function DragWrapperComponent({
+  style,
+  id,
+  position,
+  type,
+  children,
+}: PropsWithChildren<Props>) {
+  const { left, top } = position;
+  const [{ opacity }, drag] = useDrag(
+    () => ({
+      type,
+      item: { id, left, top },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.4 : 1,
+        isDragging: monitor.isDragging(),
+      }),
+    }),
+    [id, left, top, type],
+  );
+
+  return (
+    <div ref={drag} style={{ ...style, opacity }}>
+      {children}
+    </div>
+  );
+}
+
+const DragWrapper = memo(DragWrapperComponent);
+export default DragWrapper;
--- components/resume/Separator.tsx
+++ components/resume/Separator.tsx
@@ -1,38 +1,10 @@
-import { memo } from 'react';
-import { useDrag } from 'react-dnd';
 interface Props {
-  id: string;
-  type: string;
-  position: {
-    left: number;
-    top: number;
-  };
   style: {
     type: 'dotted' | 'bold';
     color: string;
   };
 }
 
-function SeparatorComponent({ style, id, position, type }: Props) {
-  const { left, top } = position;
-  const [{ opacity }, drag] = useDrag(
-    () => ({
-      type,
-      item: { id, left, top },
-      collect: (monitor) => ({
-        opacity: monitor.isDragging() ? 0.4 : 1,
-        isDragging: monitor.isDragging(),
-      }),
-    }),
-    [id, left, top, type],
-  );
-
-  return (
-    <div ref={drag} style={{ ...style, opacity }} data-testid="separator">
-      <hr className={type} style={style} />
-    </div>
-  );
+export default function Separator({ style }: Props) {
+  return <hr style={style} />;
 }
-
-const Separator = memo(SeparatorComponent);
-export default Separator;
