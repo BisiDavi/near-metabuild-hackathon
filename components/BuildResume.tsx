846842679d6f3c91aa3102a364f9a0d7ffab2dcf
BisiDavi <oludavidconnect@gmail.com>
2022-11-15 16:00:23 +0100

    resumeeditor added

--- components/BuildResume.tsx
+++
@@ -1,5 +0,0 @@
-export default function BuildResume() {
-  return (
-    <section>BuildResume</section>
-  )
-}
---
+++ components/resume/ResumeEditor.tsx
@@ -0,0 +1,7 @@
+export default function ResumeEditor() {
+  return (
+    <aside>
+      <h5>Edit Resume</h5>
+    </aside>
+  );
+}
