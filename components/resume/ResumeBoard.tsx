56a5603d980a41880a2fb98ad3383f865807658e
BisiDavi <oludavidconnect@gmail.com>
2022-11-15 09:16:12 +0100

    separator updated

--- components/resume/ResumeBoard.tsx
+++ components/resume/ResumeBoard.tsx
@@ -5,7 +5,9 @@ export default function ResumeBoard() {
   return (
     <ResumeBoardProvider>
       <section className="flex h-screen items-center bg-white rounded-lg border shadow">
-        <div className="h-full w-1/12 border-r shadow bg-white"></div>
+        <div className="h-full w-1/12 border-r rounded-l-lg shadow bg-white grid grid-rows-6 grid-cols-1">
+
+        </div>
         <div className="relative h-full bg-white w-11/12 rounded-r-lg">
           <div className="border-r-none border-b-none absolute bottom-0 right-0 h-52 w-80 bg-white rounded-br-lg border"></div>
         </div>
--- components/resume/Separator.tsx
+++ components/resume/Separator.tsx
@@ -1,15 +1,33 @@
+import { memo } from 'react';
+import { useDrag } from 'react-dnd';
 interface Props {
+  isDropped: boolean;
+  name: string;
+  type: string;
   style: {
     type: 'dotted' | 'bold';
     color: string;
   };
 }
 
-export default function Separator({ style }: Props) {
-  const { type, color } = style;
+function SeparatorComponent({ style, name, type, isDropped }: Props) {
+  const [{ opacity }, drag] = useDrag(
+    () => ({
+      type,
+      item: { name },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.4 : 1,
+      }),
+    }),
+    [name, type],
+  );
+
   return (
-    <>
-      <hr className={type} style={{ color }} />
-    </>
+    <div ref={drag} style={{ ...style, opacity }} data-testid="separator">
+      <hr className={type} style={style} />
+    </div>
   );
 }
+
+const Separator = memo(SeparatorComponent);
+export default Separator;
