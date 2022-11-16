fb90db02b7a121ec607652474151f8bf38c40d3c
BisiDavi <oludavidconnect@gmail.com>
2022-11-16 12:40:17 +0100

    resume-editor updated

--- components/resume/ResumeEditor.tsx
+++ components/resume/ResumeEditor.tsx
@@ -1,7 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function ResumeEditor() {
+  const [formStage, setFormStage] = useState(0);
+
+  function formStageHandler(type: 'prev' | 'next') {
+    const formStageState = (prevState: number) =>
+      type === 'prev' ? prevState - 1 : prevState + 1;
+    if (formStage > 0) {
+      setFormStage((prevState) => formStageState(prevState));
+    }
+  }
+
   return (
-    <aside>
-      <h5>Edit Resume</h5>
+    <aside className="mr-6">
+      <h5 className="text-center text-xl font-bold">Edit Resume</h5>
+      <form>
+        <div className="button-group mt-4 flex items-center justify-between text-white">
+          <button
+            className="rounded bg-gray-400 px-4 py-1 hover:bg-opacity-80"
+            onClick={() => formStageHandler('prev')}
+          >
+            Previous
+          </button>
+          <button
+            className="rounded bg-blue-400 px-4 py-1 hover:bg-opacity-80"
+            onClick={() => formStageHandler('next')}
+          >
+            Next
+          </button>
+        </div>
+      </form>
     </aside>
   );
 }
