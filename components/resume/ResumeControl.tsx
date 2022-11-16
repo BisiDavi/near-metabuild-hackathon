a74d3ac9f1a3177486d3239fedd824648d028f78
BisiDavi <oludavidconnect@gmail.com>
2022-11-16 20:51:04 +0100

    components deleted

--- components/resume/ResumeControl.tsx
+++
@@ -1,5 +0,0 @@
-export default function ResumeControl() {
-  return (
-    <div className="border-r-none border-b-none absolute bottom-0 right-0 h-52 w-80 rounded-br-lg border bg-white"></div>
-  );
-}
