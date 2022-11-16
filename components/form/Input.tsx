5bd8f88e1e624dc132ecffc53a3b4ead52993a70
BisiDavi <oludavidconnect@gmail.com>
2022-11-16 05:29:24 +0100

    form json added

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Input({ input }: Props) {
   return (
-    <div>
+    <div> 
       <input
         name={input.name}
         className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
