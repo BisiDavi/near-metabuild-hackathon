49bdd1a58d1974dc50aaac4cd04998aebcaa8387
BisiDavi <oludavidconnect@gmail.com>
2022-11-18 12:01:01 +0100

    resume added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,11 @@
-import dynamic from 'next/dynamic';
-
 import Layout from '@/layout';
-import ResumeEditor from '@/components/resume/ResumeEditor';
+import Resume from '@/components/resume/01/Resume';
 
-const ResumeDocument = dynamic(
-  () => import('../components/resume/01/ResumeDocument'),
-  { ssr: false },
-);
 
 export default function Home() {
   return (
     <Layout>
-      <div className="container mx-auto my-8 flex h-screen justify-center py-4">
-        <div className="w-3/12">
-          <ResumeEditor />
-        </div>
-        <div className="w-9/12">
-          <ResumeDocument />
-        </div>
-      </div>
+      <Resume />
     </Layout>
   );
 }
