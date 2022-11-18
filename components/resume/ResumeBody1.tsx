99d1bc709899227308cec6e3e5958495b372ab9d
BisiDavi <oludavidconnect@gmail.com>
2022-11-18 11:26:38 +0100

    references components added

--- components/resume/ResumeBody1.tsx
+++ components/resume/ResumeBody1.tsx
@@ -2,6 +2,7 @@ import { Text, View } from '@react-pdf/renderer';
 
 import EmploymentHistory from '@/components/resume/EmploymentHistory';
 import Education from '@/components/resume/Education';
+import References from '@/components/resume/References';
 import formatDetails from '@/lib/formatDetails';
 import { formatReferences } from '@/lib/formatReference';
 import { resumeDocumentStyle } from '@/styles/resumeDocumentStyle';
@@ -14,7 +15,6 @@ export default function ResumeBody1({ dCvs }: ResumeAside1) {
   const employmentDetails = formatDetails(employmentHistory);
   const educationDetails = formatDetails(education);
   const referencesDetails = formatReferences(references);
-  console.log('referencesDetails,', referencesDetails);
 
   return (
     <View style={styles.section}>
@@ -32,13 +32,7 @@ export default function ResumeBody1({ dCvs }: ResumeAside1) {
           <Education key={item.course} details={item} />
         ))}
       </View>
-      <View style={styles.subsection}>
-        <Text style={styles.subheading}>References</Text>
-        <Text style={styles.text}>David Leeds from Anglo Hotel</Text>
-        <Text style={styles.text}>dleeds@anglo.com | 732-189-0909</Text>
-        <Text style={styles.text}>Cole Kenny from AT&T</Text>
-        <Text style={styles.text}>kennycole@att.com | 934-289-0209</Text>
-      </View>
+      <References details={referencesDetails} />
     </View>
   );
 }
