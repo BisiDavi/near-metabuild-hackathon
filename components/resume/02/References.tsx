cbe162f250dec2700d343d76e7fb3d805470c2c9
BisiDavi <oludavidconnect@gmail.com>
2022-11-18 12:03:13 +0100

    resume 2 components added

---
+++ components/resume/02/References.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from '@react-pdf/renderer';
+
+import { resumeDocumentStyle } from '@/styles/resumeDocumentStyle';
+
+interface referenceProps {
+  details: { title: string; email: string; phone: string }[];
+}
+
+export default function References({ details }: referenceProps) {
+  const styles = resumeDocumentStyle;
+
+  return (
+    <View style={styles.subsection}>
+      <Text style={styles.heading}>References</Text>
+      {details.map((item) => (
+        <View key={item.title} style={styles.view}>
+          <Text style={styles.h6}>{item.title}</Text>
+          <Text style={styles.p}>
+            {item.email} | {item.phone}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
+}
---
+++ components/resume/02/ResumeBody.tsx
@@ -0,0 +1,28 @@
+import { View } from '@react-pdf/renderer';
+
+import EmploymentHistory from '@/components/resume/01/EmploymentHistory';
+import Education from '@/components/resume/01/Education';
+import References from '@/components/resume/01/References';
+import Profile from '@/components/resume/01/Profile';
+import formatDetails from '@/lib/formatDetails';
+import { formatReferences } from '@/lib/formatReference';
+import { resumeDocumentStyle } from '@/styles/resumeDocumentStyle';
+import type { ResumeAside1 } from '@/types/interfaces';
+
+export default function ResumeBody({ dCvs }: ResumeAside1) {
+  const styles = resumeDocumentStyle;
+  const { profile, employmentHistory, references, education } = dCvs;
+
+  const employmentDetails = formatDetails(employmentHistory);
+  const educationDetails = formatDetails(education);
+  const referencesDetails = formatReferences(references);
+
+  return (
+    <View style={styles.section}>
+      <Profile profile={profile} />
+      <EmploymentHistory history={employmentDetails} />
+      <Education details={educationDetails} />
+      <References details={referencesDetails} />
+    </View>
+  );
+}
