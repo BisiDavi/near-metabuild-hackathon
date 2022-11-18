73ac6d22688ef026d1bae07a09ffb823b7e3a748
BisiDavi <oludavidconnect@gmail.com>
2022-11-18 12:14:56 +0100

    error fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Layout from '@/layout';
-import Resume from '@/components/resume/01/Resume';
+// import Resume from '@/components/resume/01/Resume';
 import Resume2 from '@/components/resume/02/Resume';
-
 
 export default function Home() {
   return (
