e4a372cc42e41e6f0867964a49ef1a0cd7d69e9c
BisiDavi <oludavidconnect@gmail.com>
2022-11-20 00:01:40 +0100

    displayResume updated

--- lib/displayResume.tsx
+++ lib/displayResume.tsx
@@ -1,6 +1,19 @@
-import Resume from '@/components/resume/01/Resume';
-import Resume2 from '@/components/resume/02/Resume';
-import Resume3 from '@/components/resume/03/Resume';
+// import Resume from '@/components/resume/01/Resume';
+// import Resume2 from '@/components/resume/02/Resume';
+// import Resume3 from '@/components/resume/03/Resume';
+import dynamic from 'next/dynamic';
+
+const Resume = dynamic(() => import('@/components/resume/01/Resume'), {
+  ssr: false,
+});
+
+const Resume2 = dynamic(() => import('@/components/resume/02/Resume'), {
+  ssr: false,
+});
+
+const Resume3 = dynamic(() => import('@/components/resume/03/Resume'), {
+  ssr: false,
+});
 
 export default function displayResume(resume: string) {
   switch (resume) {
