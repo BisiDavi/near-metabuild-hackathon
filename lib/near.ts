f312f1d556fb61961bb57255711d0590b3ca8e8e
BisiDavi <oludavidconnect@gmail.com>
2022-11-20 04:50:44 +0100

    near function added

---
+++ lib/near.ts
@@ -0,0 +1,51 @@
+import { connect, Contract, keyStores, WalletConnection } from 'near-api-js';
+
+import environment from '@/lib/nearConfig';
+import { formatNearAmount } from 'near-api-js/lib/utils/format';
+
+const nearEnv: any = environment('testnet');
+
+export async function initializeContract() {
+  const near = await connect(
+    Object.assign(
+      {
+        deps: { keyStores: new keyStores.BrowserLocalStorageKeyStore() },
+      },
+      nearEnv,
+    ),
+  );
+  const walletConnection = new WalletConnection(near, '');
+  const accountId = walletConnection.getAccountId();
+  const contract = new Contract(
+    walletConnection.account(),
+    nearEnv.contractName,
+    {
+      viewMethods: ['get_resume', 'get_resumes'],
+      changeMethods: ['buy_resume', 'set_resume'],
+    },
+  );
+  return { accountId, contract, walletConnection };
+}
+
+export async function accountBalance() {
+  const { walletConnection } = await initializeContract();
+  return formatNearAmount(
+    (await walletConnection.account().getAccountBalance()).total,
+    2,
+  );
+}
+
+export async function getAccountId() {
+  const { walletConnection } = await initializeContract();
+  return walletConnection.getAccountId();
+}
+
+export async function login() {
+  const { walletConnection } = await initializeContract();
+  walletConnection.requestSignIn(nearEnv.contractName);
+}
+
+export async function logout() {
+  const { walletConnection } = await initializeContract();
+  walletConnection.signOut();
+}
