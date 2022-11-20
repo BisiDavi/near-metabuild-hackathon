9985bcb599e2e1e9e746b8b3ca57e13278dc44bf
BisiDavi <oludavidconnect@gmail.com>
2022-11-20 02:50:15 +0100

    contract model added

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -2,10 +2,15 @@ use near_sdk::collections::UnorderedMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen, PanicOnDefault};
 
+pub use crate::model::*;
+
+mod model;
+
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct  Marketplace {
-    resumes: UnorderedMap<String, String>
+    listed_resumes: UnorderedMap<String, model::Resume>
 }
 
 #[near_bindgen]
@@ -14,16 +19,21 @@ impl Marketplace{
     #[init]
     pub fn init() -> Self {
         Self {
-            resumes: UnorderedMap::new(b"cv".to_vec())
+            listed_resumes: UnorderedMap::new(b"listed_cv".to_vec())
         }
     }
 
-    pub fn set_resume(&mut self, id:String, product_name:String){
-        self.resumes.insert(&id, &product_name);
+    pub fn set_resume(&mut self, payload:model::Payload){
+        let resume = Resume::from_payload(payload);
+        self.listed_resumes.insert(&resume.id, &resume);
     }
 
-    pub fn get_resume(&self, id: &String) -> Option<String>{
-        self.resumes.get(id)
+    pub fn get_resume(&self, id: &String) -> Option<Resume>{
+        self.listed_resumes.get(id)
+    }
+
+    pub fn get_resumes(&self) -> Vec<Resume>{
+        return self.listed_resumes.values_as_vector().to_vec();
     }
 
 }
