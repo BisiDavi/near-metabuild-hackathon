85ceeaff42d84b683b78650453f8f406c928b1ef
BisiDavi <oludavidconnect@gmail.com>
2022-11-20 03:29:17 +0100

    contract updated

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,16 +1,12 @@
 use near_sdk::collections::UnorderedMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, PanicOnDefault};
-
-pub use crate::model::*;
-
-mod model;
-
+use near_sdk::{near_bindgen, AccountId, PanicOnDefault, env, Promise};
+use near_sdk::serde::{Serialize, Deserialize};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct  Marketplace {
-    listed_resumes: UnorderedMap<String, model::Resume>
+    listed_resumes: UnorderedMap<String, Resume>
 }
 
 #[near_bindgen]
@@ -23,7 +19,7 @@ impl Marketplace{
         }
     }
 
-    pub fn set_resume(&mut self, payload:model::Payload){
+    pub fn set_resume(&mut self, payload:Payload){
         let resume = Resume::from_payload(payload);
         self.listed_resumes.insert(&resume.id, &resume);
     }
@@ -35,5 +31,62 @@ impl Marketplace{
     pub fn get_resumes(&self) -> Vec<Resume>{
         return self.listed_resumes.values_as_vector().to_vec();
     }
+    
+    #[payable]
+    pub fn buy_resume(&mut self, resume_id: &String){
+        match self.listed_resumes.get(resume_id){
+            Some(ref mut resume) => {
+                let price = resume.price.parse().unwrap();
+                assert_eq!(env::attached_deposit(), price, "attached deposit should be equal to the product's");
+                let owner = &resume.owner.as_str();
+                Promise::new(owner.parse().unwrap()).transfer(price);
+                resume.increment_bought();
+                self.listed_resumes.insert(&resume.id, &resume);
+            }
+            _ => {
+                env::panic_str("resume not found");
+            }
+        }
+    }
 
+}
+
+#[near_bindgen]
+#[derive(Serialize, Deserialize, PanicOnDefault)]
+pub struct Payload {
+    id:String,
+    name:String,
+    image:String,
+    price:String,
+}
+
+#[near_bindgen]
+#[derive(BorshSerialize,  BorshDeserialize, Serialize, PanicOnDefault)]
+pub struct  Resume {
+    id:String,
+    name:String,
+    image:String,
+    price:String,
+    bought:u32,
+    owner: AccountId
+}
+
+ 
+
+#[near_bindgen]
+impl Resume  {
+    pub fn from_payload(payload: Payload) -> Self {
+        Self {
+            id: payload.id,
+            name: payload.name,
+            image: payload.image,
+            price: payload.price,
+            bought: 0,
+            owner: env::signer_account_id()
+        }
+    }
+
+    pub fn increment_bought(&mut self) {
+        self.bought = self.bought + 1;
+    }
 }
