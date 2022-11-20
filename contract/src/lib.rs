b891f974107b8d911b5506ae6c0b9175b549eed0
BisiDavi <oludavidconnect@gmail.com>
2022-11-20 01:56:43 +0100

    resume updated

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -5,21 +5,25 @@ use near_sdk::{near_bindgen, PanicOnDefault};
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct  Marketplace {
-    products: UnorderedMap<String, String>
+    resumes: UnorderedMap<String, String>
 }
 
 #[near_bindgen]
 impl Marketplace{
 
-#[init]
-pub fn init() -> Self {
-    Self {
-        products: UnorderedMap::new(b"product".to_vec())
+    #[init]
+    pub fn init() -> Self {
+        Self {
+            resumes: UnorderedMap::new(b"cv".to_vec())
+        }
     }
-}
 
-pub fn set_product(&mut self, id:String, product_name:String){
-    self.products.insert(&id, &product_name);
-}
+    pub fn set_resume(&mut self, id:String, product_name:String){
+        self.resumes.insert(&id, &product_name);
+    }
+
+    pub fn get_resume(&self, id: &String) -> Option<String>{
+        self.resumes.get(id)
+    }
 
 }
