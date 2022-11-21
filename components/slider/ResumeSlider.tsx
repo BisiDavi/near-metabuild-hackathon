431c1c3ea83e4b0db38990f413bdc4ff6c5eb560
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 09:28:20 +0100

    resumeslideritem added

--- components/slider/ResumeSlider.tsx
+++ components/slider/ResumeSlider.tsx
@@ -1,29 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useQuery } from '@tanstack/react-query';
-import Router from 'next/router';
 
 import useResume from '@/hooks/useResume';
-import Button from '@/components/Button';
-import { useAppDispatch } from '@/hooks/useRedux';
-import { selectResume } from '@/redux/resume-slice';
-import formatResumePrice from '@/lib/formatResumePrice';
-import selectSeeder from '@/lib/selectSeeder';
-import type { resumeStateType } from '@/types/redux-types';
 import type { resumeType } from '@/types';
 import '@splidejs/react-splide/css';
+import ResumeSliderItem from './ResumeSliderItem';
 
 export default function ResumeSlider() {
   const { fetchResumes } = useResume();
   const { status, data } = useQuery(['fetchResumes'], fetchResumes);
-  const dispatch = useAppDispatch();
-
-  function selectResumeHandler(type: resumeStateType['selectedResume']) {
-    console.log('type', type);
-    const selectedSeeder = selectSeeder(type);
-    Router.push(`/template/${type}`);
-    return dispatch(selectResume({ type, seeder: selectedSeeder }));
-  }
 
   return (
     <div id="get-started" className="relative">
@@ -42,28 +28,11 @@ export default function ResumeSlider() {
           ? 'unable to fetch resumes'
           : status === 'loading'
           ? 'loading, fetching resumes ...'
-          : data.map((item: resumeType) => {
-              const price = formatResumePrice(item.price);
-              return (
-                <SplideSlide key={item.name}>
-                  <div className="relative mx-auto flex flex-col items-center justify-center">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="z-10 rounded-md"
-                    />
-                    <Button
-                      className="absolute z-50 mx-auto flex items-center justify-center rounded-lg bg-blue-500 px-4 py-1 text-white hover:bg-opacity-80"
-                      text="Use this Template"
-                      onClick={() => selectResumeHandler(item.id)}
-                    />
-                    <div className="absolute bottom-2 z-10 rounded-md bg-gray-400 px-2 py-0.5 text-sm text-white">
-                      Price: {price} NEAR
-                    </div>
-                  </div>
-                </SplideSlide>
-              );
-            })}
+          : data.map((item: resumeType) => (
+              <SplideSlide key={item.name}>
+                <ResumeSliderItem item={item} />
+              </SplideSlide>
+            ))}
       </Splide>
     </div>
   );
---
+++ components/slider/ResumeSliderItem.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import Router from 'next/router';
+
+import useResume from '@/hooks/useResume';
+import { useAppDispatch } from '@/hooks/useRedux';
+import Button from '@/components/Button';
+import selectSeeder from '@/lib/selectSeeder';
+import formatResumePrice from '@/lib/formatResumePrice';
+import { selectResume } from '@/redux/resume-slice';
+import type { resumeStateType } from '@/types/redux-types';
+import type { resumeType } from '@/types';
+
+interface Props {
+  item: resumeType;
+}
+
+export default function ResumeSliderItem({ item }: Props) {
+  const { fetchResumes } = useResume();
+  const dispatch = useAppDispatch();
+  const price = formatResumePrice(item.price);
+
+  function selectResumeHandler(type: resumeStateType['selectedResume']) {
+    console.log('type', type);
+    const selectedSeeder = selectSeeder(type);
+    Router.push(`/template/${type}`);
+    return dispatch(selectResume({ type, seeder: selectedSeeder }));
+  }
+  return (
+    <div className="relative mx-auto flex flex-col items-center justify-center">
+      <img src={item.image} alt={item.name} className="z-10 rounded-md" />
+      <Button
+        className="absolute z-50 mx-auto flex items-center justify-center rounded-lg bg-blue-500 px-4 py-1 text-white hover:bg-opacity-80"
+        text="Use this Template"
+        onClick={() => selectResumeHandler(item.id)}
+      />
+      <div className="absolute bottom-2 z-10 rounded-md bg-gray-400 px-2 py-0.5 text-sm text-white">
+        Price: {price} NEAR
+      </div>
+    </div>
+  );
+}
