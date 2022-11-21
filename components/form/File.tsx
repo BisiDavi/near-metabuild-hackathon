190a61a2104c8a7b776bb834780ac356be3ce49e
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 18:47:46 +0100

    resume updated

--- components/form/File.tsx
+++ components/form/File.tsx
@@ -1,8 +1,11 @@
 import useMediaUpload from '@/hooks/useMediaUpload';
 import type { FormElementProps } from '@/types/interfaces';
+import { useRouter } from 'next/router';
 
 export default function File({ input }: FormElementProps) {
   const { uploadMedia } = useMediaUpload();
+  const router = useRouter();
+  const templateId = router.asPath.split('/template/')[1];
 
   function onClickHandler(e: any) {
     if (e.target.files) {
@@ -12,13 +15,17 @@ export default function File({ input }: FormElementProps) {
 
   return (
     <>
-      <p className="mb-0">Upload your profile picture</p>
-      <input
-        type="file"
-        name={input.name}
-        className="my-1 w-full rounded-md border border-gray-300 p-2 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
-        onChange={onClickHandler}
-      />
+      {input.placeholder.includes(templateId) && (
+        <>
+          <p className="mb-0">Upload your profile picture</p>
+          <input
+            type="file"
+            name={input.name}
+            className="my-1 w-full rounded-md border border-gray-300 p-2 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
+            onChange={onClickHandler}
+          />
+        </>
+      )}
     </>
   );
 }
