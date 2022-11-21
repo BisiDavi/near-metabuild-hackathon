9ae494aca3a126b0003445a557094d297a8e4ad4
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 18:39:54 +0100

    selectSeeder updated

--- lib/selectSeeder.ts
+++ lib/selectSeeder.ts
@@ -2,6 +2,7 @@ import { resumeStateType } from '@/types/redux-types';
 import { seeder1 } from '@/lib/seeder-1';
 import { seeder2 } from '@/lib/seeder-2';
 import { seeder3 } from '@/lib/seeder-3';
+import { seeder4 } from '@/lib/seeder-4';
 
 export default function selectSeeder(
   resumeType: resumeStateType['selectedResume'],
@@ -13,5 +14,7 @@ export default function selectSeeder(
       return seeder2;
     case 'resume-3':
       return seeder3;
+    case 'resume-4':
+      return seeder4;
   }
 }
