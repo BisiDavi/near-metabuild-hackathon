0d9895347d2719477cfe656e32776ad6367a805a
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 02:41:50 +0100

    resume updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import HeroWave from '@/components/HeroWave';
 import HeroCards from '@/components/HeroCards';
 import heroContent from '@/json/hero.json';
 
 export default function Hero() {
+  const router = useRouter();
+  const routerPath = router.asPath
   return (
     <section className="hero">
       <div className="content bg-skyblue h-52">
         <div className="container mx-auto flex flex-col py-20 text-center text-white">
           <h1 className="text-3xl">{heroContent.home.main} </h1>
           <h6 className="text-lg">{heroContent.home.info}</h6>
-          <Link href="/#get-started" scroll={true}>
+          {!routerPath.includes("cv") &&<Link href="/#get-started" scroll={true}>
             <button className="0 relative my-8 mx-auto flex w-auto rounded-md bg-white px-4 py-1.5 font-bold text-blue-500 hover:bg-gray-200">
               Get Started
               <span className="absolute -right-1.5 -top-1 inline-flex h-3  w-3 animate-ping rounded-full bg-red-500"></span>
               <span className="absolute -right-1.5 -top-1 inline-flex h-3 w-3 rounded-full bg-red-500"></span>
             </button>
-          </Link>
+          </Link>}
         </div>
       </div>
       <HeroWave />
--- pages/cv/rate.tsx
+++ pages/cv/rate.tsx
@@ -1,9 +1,41 @@
+import Link from 'next/link';
 import Layout from '@/layout';
 
 export default function Rate() {
   return (
     <Layout>
-      <div>rate</div>
+      <section className="container mx-auto py-4">
+        <h4 className="mt-4 text-center text-3xl font-bold">
+          Rate our resumé and get rewarded with 0.1 NEAR
+        </h4>
+        <div className="how mt-4">
+          <h6 className="text-xl font-bold underline">How to earn</h6>
+          <ul>
+            <li className="my-1">
+              1. Select a resumé template from the{' '}
+              <Link
+                href="/#get-started"
+                className="font-bold text-blue-500 hover:text-red-500"
+              >
+                homepage
+              </Link>
+            </li>
+            <li className="my-1">
+              2. Fill the resumé template with your details
+            </li>
+            <li className="my-1">
+              3. Fill the resumé template with your details
+            </li>
+            <li className="my-1">
+              4. Make payment for the resumé template and download your resumé
+            </li>
+            <li className="my-1">
+              5. Rate the resumé template used and get 0.1 NEAR transferred to
+              your test wallet
+            </li>
+          </ul>
+        </div>
+      </section>
     </Layout>
   );
 }
