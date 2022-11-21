747eb46a981b028bb1bc262002df5ff0b71bc7f6
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 04:16:05 +0100

    payment updated

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -4,9 +4,12 @@ import NEAROverlay from '@/components/NEAROverlay';
 import Layout from '@/layout';
 import Button from '@/components/Button';
 import usePayment from '@/hooks/usePayment';
+import displayResume from '@/lib/displayResume';
+import { useAppSelector } from '@/hooks/useRedux';
 
 export default function Payment() {
   const { price, nearData, nearStatus, data, status, mutation } = usePayment();
+  const { selectedResume } = useAppSelector((state) => state.resume);
 
   return (
     <Layout showHero={false}>
@@ -22,23 +25,26 @@ export default function Payment() {
           ? 'loading, fetching payment details...'
           : data && (
               <div className="content payment-view flex h-full w-full items-center">
-                <div className="mx-auto  flex h-full w-3/4 flex-col items-center justify-center">
-                  <h4 className="text-center text-4xl text-white">
+                <div className="mx-auto  flex h-full w-3/12 flex-col items-center bg-gray-400 px-4 py-20">
+                  <h4 className="border p-2 text-center text-lg text-white">
                     Thanks for using {data.name} template from{' '}
                     <span className="font-bold">Near-Resumé</span> in making
                     your Resumé, make payment to download the resume
                   </h4>
-                  <p className="my-4 text-2xl font-bold text-white">
+                  <p className="my-4 text-lg font-bold text-white">
                     Pay with NEAR testnet token
                   </p>
                   <Button
                     text={`Make Payment of ${price} NEAR`}
-                    className="my-3 text-xl rounded bg-green-500 px-2 py-1 text-white hover:bg-green-900"
+                    className="my-3 rounded bg-green-500 px-4 py-1 font-bold  text-white hover:bg-green-900"
                     onClick={() =>
                       mutation.mutate({ id: data.id, price: data.price })
                     }
                   />
                 </div>
+                {selectedResume && (
+                  <div className="w-9/12">{displayResume(selectedResume)}</div>
+                )}
               </div>
             )}
       </section>
