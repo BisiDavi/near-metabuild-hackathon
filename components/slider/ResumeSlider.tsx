53aa1a7a04f9d76401b03321111966dd0a7ed173
BisiDavi <oludavidconnect@gmail.com>
2022-11-21 11:03:28 +0100

    slider udpated

--- components/slider/ResumeSlider.tsx
+++ components/slider/ResumeSlider.tsx
@@ -1,39 +1,37 @@
-/* eslint-disable @next/next/no-img-element */
-import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useQuery } from '@tanstack/react-query';
+import Slider from 'react-slick';
 
 import useResume from '@/hooks/useResume';
+import ResumeSliderItem from '@/components/slider/ResumeSliderItem';
 import type { resumeType } from '@/types';
-import '@splidejs/react-splide/css';
-import ResumeSliderItem from './ResumeSliderItem';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function ResumeSlider() {
   const { fetchResumes } = useResume();
   const { status, data } = useQuery(['fetchResumes'], fetchResumes);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+
   return (
-    <div id="get-started" className="relative">
-      <Splide
-        options={{
-          autoplay: false,
-          perPage: 3,
-          gap: '2rem',
-          perMove: 1,
-          type: 'loop',
-        }}
-        aria-label="resumes"
-        className="my-14 flex w-full bg-blue-400 p-10"
-      >
-        {status === 'error'
-          ? 'unable to fetch resumes'
-          : status === 'loading'
-          ? 'loading, fetching resumes ...'
-          : data.map((item: resumeType) => (
-              <SplideSlide key={item.name}>
-                <ResumeSliderItem item={item} />
-              </SplideSlide>
-            ))}
-      </Splide>
+    <div id="get-started" className="resume-grid my-6 relative">
+      {data && (
+        <Slider {...settings}>
+          {status === 'error'
+            ? 'unable to fetch resumes'
+            : status === 'loading'
+            ? 'loading, fetching resumes ...'
+            : data.map((item: resumeType) => (
+                <ResumeSliderItem key={item.name} item={item} />
+              ))}
+        </Slider>
+      )}
     </div>
   );
 }
--- components/slider/ResumeSliderItem.tsx
+++ components/slider/ResumeSliderItem.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Router from 'next/router';
 
-import useResume from '@/hooks/useResume';
 import { useAppDispatch } from '@/hooks/useRedux';
 import Button from '@/components/Button';
 import selectSeeder from '@/lib/selectSeeder';
@@ -15,7 +14,6 @@ interface Props {
 }
 
 export default function ResumeSliderItem({ item }: Props) {
-  const { fetchResumes } = useResume();
   const dispatch = useAppDispatch();
   const price = formatResumePrice(item.price);
 
@@ -26,7 +24,7 @@ export default function ResumeSliderItem({ item }: Props) {
     return dispatch(selectResume({ type, seeder: selectedSeeder }));
   }
   return (
-    <div className="relative mx-auto flex flex-col items-center justify-center">
+    <div className="relative  mx-3 rounded flex flex-col items-center justify-center border">
       <img src={item.image} alt={item.name} className="z-10 rounded-md" />
       <Button
         className="absolute z-50 mx-auto flex items-center justify-center rounded-lg bg-blue-500 px-4 py-1 text-white hover:bg-opacity-80"
