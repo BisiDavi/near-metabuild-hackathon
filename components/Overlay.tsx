ee4447bf6acb2285c41a9cd9d0a11b6e9851d0a1
BisiDavi <oludavidconnect@gmail.com>
2022-11-22 16:53:09 +0100

    hook updated

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -24,11 +24,11 @@ export default function Overlay() {
           className={`overlay ${displayOverlay} absolute z-20 flex h-full w-full flex-col items-center justify-center bg-gray-500 bg-opacity-80`}
         >
           <h4 className="z-40 text-center text-4xl font-bold text-white opacity-100">
-            Please Login to continue
+            Please login to continue
           </h4>
           <Button
             text="Login with Google"
-            className="my-2 flex items-center rounded-full border bg-white px-6 py-2 text-xl font-bold text-blue-500 hover:bg-gray-300"
+            className="my-5 flex items-center rounded-full border bg-white px-6 py-2 text-xl font-bold text-blue-500 hover:bg-gray-300"
             iconUrl="/google-icon.png"
             onClick={googleHandler}
           />
