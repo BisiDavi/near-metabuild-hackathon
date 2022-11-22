ce0f0141521fcb4dde8b017e8a8372cd727b03b0
BisiDavi <oludavidconnect@gmail.com>
2022-11-22 16:12:42 +0100

    resume updated

--- components/resume/04/Resume.tsx
+++ components/resume/04/Resume.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 
 import ResumeEditor from '@/components/resume/common/ResumeEditor';
+import Overlay from '@/components/Overlay';
 
 const ResumeDocument = dynamic(
   () => import('@/components/resume/04/ResumeDocument'),
@@ -9,13 +10,16 @@ const ResumeDocument = dynamic(
 
 export default function Resume() {
   return (
-    <div className="container mx-auto flex h-screen justify-center py-4">
-      <div className="w-3/12">
-        <ResumeEditor colorPicker={false} />
+    <>
+      <Overlay />
+      <div className="container mx-auto flex h-screen justify-center py-4">
+        <div className="w-3/12">
+          <ResumeEditor colorPicker={false} />
+        </div>
+        <div className="w-9/12">
+          <ResumeDocument />
+        </div>
       </div>
-      <div className="w-9/12">
-        <ResumeDocument />
-      </div>
-    </div>
+    </>
   );
 }
