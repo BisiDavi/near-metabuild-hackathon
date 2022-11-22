6e69b42843c4cdec813521de9df6d1cceca55e5c
BisiDavi <oludavidconnect@gmail.com>
2022-11-22 14:45:29 +0100

    send-email api added

---
+++ pages/api/send-email.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function sendEmailHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  switch (req.method) {
+    case 'POST': {
+        
+    }
+  }
+}
