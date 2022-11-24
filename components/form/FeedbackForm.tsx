af1db261dd10472b279815cf59a73939815bb132
BisiDavi <oludavidconnect@gmail.com>
2022-11-24 09:01:51 +0100

    feedbackform updated

--- components/form/FeedbackForm.tsx
+++ components/form/FeedbackForm.tsx
@@ -4,20 +4,38 @@ import Button from '@/components/Button';
 import RatingStar from '@/components/RatingStar';
 
 export default function FeedbackForm() {
-  const [feedback, setFeedback] = useState('');
+  const [formData, setFormData] = useState({
+    feedback: '',
+  });
   const [ratings, setRatings] = useState(0);
 
+  function onChangeHandler(e: any) {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function onSubmitHandler(e: any) {
+    e.peventDefault();
+    setFormData((prev) => ({
+      ...prev,
+      ratings,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <h4>
         How was the template during usage, Rate this template and give us a
         feedback to earn 0.1 NEAR
       </h4>
       <RatingStar ratings={ratings} setRatings={setRatings} />
       <textarea
-        value={feedback}
+        value={formData.feedback}
+        name="feedback"
         placeholder="we will love to hear your feedback"
-        onChange={(e) => setFeedback(e.target.value)}
+        onChange={onChangeHandler}
       ></textarea>
       <Button type="submit" text="Submit" className="" onClick={() => null} />
     </form>
